14b570a959787a9f17adf8f46e7003e2a13ca725
vlad07072002li@gmail.com <vlad07072002li@gmail.com>
2020-05-31 17:54:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+int main()
+{
+	std::string str = "bdfbdfbavnbbfbb";
+	std::string word;
+	std::istringstream ins(str);
+	int count = 0;
+	while (ins >> word)
+		if (word[0] == 'b') count++;
+	std::cout<<"The number of words starting with the letters b means:" << count << std::endl;
+	return 0;
+}
